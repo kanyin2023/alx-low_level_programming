e31c389c8174c0aa22c30d04445a9ebd3d65f53e
kanyin2023 <folorunsomaryam@gmail.com>
2023-09-01 17:06:37 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,10 @@
 
 /**
  * main- a program that prints its name
+ *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
+ *
  * Return: Always 0 (Success)
 */
 
